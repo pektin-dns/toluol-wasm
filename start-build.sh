2d134fa745561f74a8d06c4562bbfcb072876d52
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-11-03 22:13:41 +0100

    v

--- start-build.sh
+++ start-build.sh
@@ -1,3 +1,3 @@
 mkdir pkg
-docker run --env UID=$(id -u) --env GID=$(id -g) --name wasm-build --mount "type=bind,source=$PWD/pkg/,dst=/pkg/" -i $(docker build -q .)
+docker run --env UID=$(id -u) --env GID=$(id -g) --name wasm-build --mount "type=bind,source=$PWD/pkg,dst=/pkg" -i $(docker build -q .)
 
