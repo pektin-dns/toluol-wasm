dbbc980284530044f67789ffe8b4e610abf2d1b2
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-02-02 17:11:16 +0100

    new package builds

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-wasm-pack build
-wasm-opt -Oz -o pkg/toluol_wasm_bg.wasm pkg/toluol_wasm_bg.wasm
-du -hs pkg/
+wasm-pack build --release --target web --out-dir dist/web
+wasm-opt -Oz -o dist/web/toluol_wasm_bg.wasm dist/web/toluol_wasm_bg.wasm
+sed -i "s|toluol-wasm|@pektin/toluol-wasm-web|g" dist/web/package.json
+
+wasm-pack build --release --target nodejs --out-dir dist/nodejs
+wasm-opt -Oz -o dist/nodejs/toluol_wasm_bg.wasm dist/nodejs/toluol_wasm_bg.wasm
+sed -i "s|toluol-wasm|@pektin/toluol-wasm-nodejs|g" dist/nodejs/package.json
+
+wasm-pack build --release --target bundler --out-dir dist/bundler
+wasm-opt -Oz -o dist/bundler/toluol_wasm_bg.wasm dist/bundler/toluol_wasm_bg.wasm
+sed -i "s|toluol-wasm|@pektin/toluol-wasm-bundler|g" dist/bundler/package.json
